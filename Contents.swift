dfabb3b3b242789a1abc8391ec0237a0dbbb46b8
Mwai Banda <49708426+Mwaib1@users.noreply.github.com>
2021-03-15 20:00:18 -0500

    Add files via upload

---
+++ Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class person {
+    var name = "Mwai"
+}
+
+struct personStruct {
+    var  name = "Mwai"
+}
+
+
+var p1 = person()
+var p2 = personStruct()
+
+var p1Copy = p1
+var p2Copy = p2
+
+p1Copy.name = "Banda"
+p2Copy.name = "Banda"
+
+print("p1", p1.name)
+print("p2", p2.name)
+print("p1Copy", p1Copy.name)
+print("p2Copy", p2Copy.name)
